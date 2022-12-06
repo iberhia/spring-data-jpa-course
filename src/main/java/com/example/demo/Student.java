e06b462e7e196f4c7d9dc3cbab7906aa59c818a6
vitocheng <vitocheng@jintengtech.com>
2022-12-06 15:59:29 +0800

    p12

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,23 +1,74 @@
 package com.example.demo;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
 //使用java
 @Entity(name = "Student")
+//自定义sequence generator：主键生成器，@Target({TYPE, METHOD, FIELD})
+@SequenceGenerator(
+        //表示该表主键生成策略名称，它被引用在@GeneratedValue中设置的“gernerator”值中
+        name = "generator_name",
+        //表示生成策略用到的数据库序列名称
+        sequenceName = "sequence_name",
+        //每次主键值增加的大小
+        allocationSize = 1,
+        //表示主键初始值，默认为1
+        initialValue = 50
+)
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = {"email"})
+        }
+)
 public class Student {
 
     //Primary Key
     @Id
+
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            //引用的generator
+            generator = "generator_name"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
 
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
 
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
 
+    @Column(
+            name = "email",
+            nullable = false
+    )
     private String email;
 
+    @Column(
+            name = "age",
+            nullable = false
+    )
     private Integer age;
 
     public Student(Long id,
