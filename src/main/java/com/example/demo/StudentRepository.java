38b2218612d785f249063584fc9dd26ded46b9ec
vitocheng <vitocheng@jintengtech.com>
2022-12-07 15:12:48 +0800

    p24：@Query Native Queries

--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -23,8 +23,12 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("select s from StudentEntityName s where s.firstName = ?1 and s.age >= ?2")
     List<Student> findStudentByFirstNameEqualsAndAgeIsGreaterThanEqual(String firstName, Integer age);
 
-    @Query("select s from StudentEntityName s where s.firstName = ?1 and s.age >= ?2")
-    //@Query指定语句之后，可以自定义方法名字，不需要按jpa的自动生成方法名
+//    @Query("select s from StudentEntityName s where s.firstName = ?1 and s.age >= ?2")
+//    //@Query指定语句之后，可以自定义方法名字，不需要按jpa的自动生成方法名
+//    List<Student> selectStudentByFirstNameAndAgeGreaterEqual(String firstName, Integer age);
+
+
+    @Query(value = "select * from amigoscode.student where first_name= ?1 and age>= ?2", nativeQuery = true)
     List<Student> selectStudentByFirstNameAndAgeGreaterEqual(String firstName, Integer age);
 
     List<Student> findStudentByFirstNameEqualsAndAgeEquals(String firstName, Integer age);
