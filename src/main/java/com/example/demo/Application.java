1dda2e34398e884c11ff5a38e21d62bbbf736728
vitocheng <vitocheng@jintengtech.com>
2022-12-07 11:14:14 +0800

    p20-p21：Repository自定义查询方法

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -23,28 +23,50 @@ public class Application {
                     "Jones",
                     "maria.jones@amigoscode.edu",
                     21);
+            Student maria2 = new Student("Maria",
+                    "Jones",
+                    "maria2.jones@amigoscode.edu",
+                    21);
+            Student maria3 = new Student("Maria",
+                    "Jones",
+                    "maria3.jones@amigoscode.edu",
+                    22);
             Student z3 = new Student("San",
                     "Zhang",
                     "z3@zju.edu",
                     28);
             students.add(maria);
+            students.add(maria2);
+            students.add(maria3);
             students.add(z3);
             System.out.println("Insert all students");
             studentRepository.saveAll(students);
-            System.out.println("all students count");
-            System.out.println(studentRepository.count());
-            studentRepository.findById(51L).ifPresentOrElse(
-                    System.out::println,
-                    () -> System.out.println("Student with ID=51 not found"));
-            studentRepository.findById(2L).ifPresentOrElse(
-                    System.out::println,
-                    () -> System.out.println("Student with ID=2 not found"));
-            final List<Student> all = studentRepository.findAll();
-            System.out.println("all students");
-            all.forEach(System.out::println);
-            System.out.println("delete maria");
-            studentRepository.deleteById(50L);
-            System.out.println("Student count = " + studentRepository.count());
+
+//            System.out.println("all students count");
+//            System.out.println(studentRepository.count());
+//            studentRepository.findById(51L).ifPresentOrElse(
+//                    System.out::println,
+//                    () -> System.out.println("Student with ID=51 not found"));
+//            studentRepository.findById(2L).ifPresentOrElse(
+//                    System.out::println,
+//                    () -> System.out.println("Student with ID=2 not found"));
+//            final List<Student> all = studentRepository.findAll();
+//            System.out.println("all students");
+//            all.forEach(System.out::println);
+//            System.out.println("delete maria");
+//            studentRepository.deleteById(50L);
+//            System.out.println("Student count = " + studentRepository.count());
+
+            studentRepository
+                    .findStudentByEmail("z3@zju.edu")
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with email z3@zju.edu Not found"));
+
+            studentRepository
+                    .findStudentByFirstNameEqualsAndAgeEquals("maria", 21)
+                    .forEach(System.out::println);
+
         };
     }
 
--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -2,7 +2,18 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 //JpaRepository<T, ID> T:Entity, ID:PK Type
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    /**
+     *     un pk等：optional，其他返回list，因为可能有多个
+     * @param email email
+     * @return Optional
+     */
+    Optional<Student> findStudentByEmail(String email);
+
+    List<Student> findStudentByFirstNameEqualsAndAgeEquals(String firstName, Integer age);
 }
