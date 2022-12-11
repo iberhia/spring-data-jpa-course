5b4b248504b9b6faee4894847cb4861b5990ddd0
vitocheng <vitocheng@jintengtech.com>
2022-12-11 19:46:11 +0800

    p30-31 paging and sorting

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
@@ -20,15 +22,25 @@ public class Application {
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
             generateRandomStudents(studentRepository);
-            //firstName: java class property name
-//            final Sort firstName = Sort.by(Sort.Direction.ASC, "firstName");
-            final Sort firstName = Sort.by("firstName").ascending()
-                    .and(Sort.by("age").descending());
-
-            studentRepository
-                    .findAll(firstName)
-                    .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+            sorting(studentRepository);
+            PageRequest pageRequest = PageRequest.of(0,
+                    5,
+                    Sort.by("firstName").ascending()
+                            .and(Sort.by("age").descending()));
+            final Page<Student> studentPage = studentRepository.findAll(pageRequest);
+            System.out.println("studentPage:" + studentPage);
         };
+    }
+
+    private void sorting(StudentRepository studentRepository) {
+        //firstName: java class property name
+//            final Sort firstName = Sort.by(Sort.Direction.ASC, "firstName");
+        final Sort firstName = Sort.by("firstName").ascending()
+                .and(Sort.by("age").descending());
+
+        studentRepository
+                .findAll(firstName)
+                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
     }
 
     private void generateRandomStudents(StudentRepository studentRepository) {
