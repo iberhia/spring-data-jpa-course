5db62f0c922989a75810259072bedc082719ce69
vitocheng <vitocheng@jintengtech.com>
2022-12-11 19:31:04 +0800

    p29 sort

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,76 +19,27 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            List<Student> students = new ArrayList<>();
-            Student maria = new Student("Maria",
-                    "Jones",
-                    "maria.jones@amigoscode.edu",
-                    21);
-            Student maria2 = new Student("Maria",
-                    "Jones",
-                    "maria2.jones@amigoscode.edu",
-                    21);
-            Student maria3 = new Student("Maria",
-                    "Jones",
-                    "maria3.jones@amigoscode.edu",
-                    22);
-            Student z3 = new Student("San",
-                    "Zhang",
-                    "z3@zju.edu",
-                    28);
-            students.add(maria);
-            students.add(maria2);
-            students.add(maria3);
-            students.add(z3);
-            System.out.println("Insert all students");
-            studentRepository.saveAll(students);
-
-//            System.out.println("all students count");
-//            System.out.println(studentRepository.count());
-//            studentRepository.findById(51L).ifPresentOrElse(
-//                    System.out::println,
-//                    () -> System.out.println("Student with ID=51 not found"));
-//            studentRepository.findById(2L).ifPresentOrElse(
-//                    System.out::println,
-//                    () -> System.out.println("Student with ID=2 not found"));
-//            final List<Student> all = studentRepository.findAll();
-//            System.out.println("all students");
-//            all.forEach(System.out::println);
-//            System.out.println("delete maria");
-//            studentRepository.deleteById(50L);
-//            System.out.println("Student count = " + studentRepository.count());
+            generateRandomStudents(studentRepository);
+            //firstName: java class property name
+//            final Sort firstName = Sort.by(Sort.Direction.ASC, "firstName");
+            final Sort firstName = Sort.by("firstName").ascending()
+                    .and(Sort.by("age").descending());
 
             studentRepository
-                    .findStudentByEmail("z3@zju.edu")
-                    .ifPresentOrElse(
-                            System.out::println,
-                            () -> System.out.println("Student with email z3@zju.edu Not found"));
-
-            studentRepository
-                    .findStudentByFirstNameEqualsAndAgeEquals("maria", 21)
-                    .forEach(System.out::println);
-
-            studentRepository
-                    .findStudentByFirstNameEqualsAndAgeIsGreaterThanEqual("maria", 21)
-                    .forEach(System.out::println);
-
-            studentRepository
-                    .selectStudentByFirstNameAndAgeGreaterEqualNative("maria", 21)
-                    .forEach(System.out::println);
-
-            studentRepository
-                    .selectStudentByFirstNameAndAgeGreaterEqualNativeNamedParams("maria", 21)
-                    .forEach(System.out::println);
-
-            System.out.println("delete maria2");
-
-            System.out.println("student deleted count: " + studentRepository.deleteStudentById(51L));
-
-            studentRepository
-                    .selectStudentByFirstNameAndAgeGreaterEqualNativeNamedParams("maria", 21)
-                    .forEach(System.out::println);
-
+                    .findAll(firstName)
+                    .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
         };
+    }
+
+    private void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 100; i++) {
+            final String firstName = faker.name().firstName();
+            final String lastName = faker.name().lastName();
+            final String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            final Student student = new Student(firstName, lastName, email, faker.number().numberBetween(0, 100));
+            studentRepository.save(student);
+        }
     }
 
 }
