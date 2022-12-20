f85bbc4592c5dbd3f546ba8b3116f9c840bd8ecf
vitocheng <vitocheng@jintengtech.com>
2022-12-20 18:48:40 +0800

    p42 foreign key

--- src/main/java/com/example/demo/StudentIdCard.java
+++ src/main/java/com/example/demo/StudentIdCard.java
@@ -4,6 +4,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -53,7 +54,10 @@ public class StudentIdCard {
             fetch = FetchType.EAGER)
     @JoinColumn(
             name = "student_id",
-            referencedColumnName = "id"
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "student_id_fk"
+            )
     )
     private Student student;
 
